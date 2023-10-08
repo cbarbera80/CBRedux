502a2e22804aece0bee3b9ef61a6359dccffa8ca
Claudio Barbera <barbera.claudio@gmail.com>
2023-10-08 16:27:29 +0200

    Better logic

--- Sources/CBRedux/CBRedux.swift
+++ Sources/CBRedux/CBRedux.swift
@@ -1,37 +1,68 @@
 import Foundation
 
-public typealias Middleware<State> = (_ state: State, _ action: Action) async throws -> Action?
-
 public protocol Action {}
 
-public typealias Reducer<State> = (_ oldState: State, _ with: Action) -> State
+public protocol ReducerProtocol {
+    associatedtype StateType
+    func reduce(oldState: StateType, with action: Action) -> StateType
+}
+
+public protocol MiddlewareProtocol {
+    associatedtype StateType
+    func process(state: StateType, with action: Action) async throws -> Action?
+}
+
+public struct AnyReducer<State>: ReducerProtocol {
+    private let _reduce: (State, Action) -> State
+
+    public init<R: ReducerProtocol>(_ reducer: R) where R.StateType == State {
+        self._reduce = reducer.reduce
+    }
+
+    public func reduce(oldState: State, with action: Action) -> State {
+        return _reduce(oldState, action)
+    }
+}
+
+public struct AnyMiddleware<State>: MiddlewareProtocol {
+    private let _process: (State, Action) async throws -> Action?
+
+    public init<M: MiddlewareProtocol>(_ middleware: M) where M.StateType == State {
+        self._process = middleware.process
+    }
+
+    public func process(state: State, with action: Action) async throws -> Action? {
+        return try await _process(state, action)
+    }
+}
 
 public final class Store<State>: ObservableObject {
     @Published public private(set) var state: State
-    private var reducer: Reducer<State>
-    private var middlewares: [Middleware<State>]
-
-    public init(
+    private var reducer: AnyReducer<State>
+    private var middlewares: [AnyMiddleware<State>]
+    
+    
+    public init<R: ReducerProtocol, M: MiddlewareProtocol>(
         initialState: State,
-        reducer: @escaping Reducer<State>,
-        middlewares: [Middleware<State>] = []
-    ) {
+        reducer: R,
+        middlewares: [M] = []
+    ) where R.StateType == State, M.StateType == State {
         self.state = initialState
-        self.reducer = reducer
-        self.middlewares = middlewares
+        self.reducer = AnyReducer(reducer)
+        self.middlewares = middlewares.map(AnyMiddleware.init)
     }
-
+    
     @MainActor
     public func dispatch(action: Action) async throws {
-        state = reducer(state, action)
-
+        state = reducer.reduce(oldState: state, with: action)
+        
         try await withThrowingTaskGroup(of: Action?.self) { group in
-            middlewares.forEach { middleware in
+            for middleware in middlewares {
                 _ = group.addTaskUnlessCancelled {
-                    try await middleware(self.state, action)
+                    try await middleware.process(state: self.state, with: action)
                 }
             }
-
+            
             for try await case let nextAction? in group where !Task.isCancelled {
                 try await dispatch(action: nextAction)
             }
