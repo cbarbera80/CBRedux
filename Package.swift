e51908bfed56f4344781c699696d94e2fc178c2b
Claudio Barbera <barbera.claudio@gmail.com>
2023-10-08 11:02:49 +0200

    first commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CBRedux",
+    platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v5)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
--- Sources/CBRedux/CBRedux.swift
+++ Sources/CBRedux/CBRedux.swift
@@ -1,2 +1,46 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public typealias Middleware<State> = (
+    @escaping (Action) -> Void,
+    @escaping () -> State?,
+    Action
+) async -> Void
+
+public protocol Action {}
+
+public typealias Reducer<State> = (inout State, Action) async -> Void
+
+public final class Store<State> {
+    private var state: State
+    private var reducer: Reducer<State>
+    private var middlewares: [Middleware<State>]
+
+    public init(
+        initialState: State,
+        reducer: @escaping Reducer<State>,
+        middlewares: [Middleware<State>] = []
+    ) {
+        self.state = initialState
+        self.reducer = reducer
+        self.middlewares = middlewares
+    }
+
+    public func dispatch(action: Action) async {
+        // Esegui i middleware in ordine
+        for middleware in middlewares {
+            await middleware({ [weak self] action in
+                guard let selfCopy = self else { return }
+                
+                Task {
+                    await selfCopy.dispatch(action: action)
+                }
+            }, { [weak self] in self?.state }, action)
+        }
+
+        await reducer(&state, action)
+    }
+
+    public var currentState: State {
+        return state
+    }
+}
